ec257d922b15da160b4b836c023ca73ed8b79ba3
Yichen Wu <wuyichen1987@gmail.com>
2020-08-10 23:17:10 +0100

    Update emailSender.py

--- emailSender.py
+++ emailSender.py
@@ -5,14 +5,14 @@ from email.message import EmailMessage
 
 email = EmailMessage()
 email['from'] = 'yichen'
-email['to'] = 'takehisa1900@gmail.com'
+email['to'] = '@gmail.com'
 email['subject'] = 'love letter'
 
 html = Template(Path('index.html').read_text())
-email.set_content(html.substitute({'name': 'zexi'}), 'html')
+email.set_content(html.substitute({'name': ''}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('wuyichen1987@gmail.com', 'Wyc8430507208')
+    smtp.login('@gmail.com', '')
     smtp.send_message(email)
